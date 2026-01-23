c8d61713ba970747b051932d48299516f0a0ca11
Amine Yagoub <yagoub.2.amine@gmail.com>
2026-01-23 18:02:43 +0100

    fix(prisma): export all types from generated client for better type access

--- libs/prisma/src/index.ts
+++ libs/prisma/src/index.ts
@@ -1,4 +1,4 @@
 export { PrismaClient } from './generated/client';
-export type { User, JobStatus, AnalysisJob } from './generated/client';
+export * from './generated/client';
 export * from './lib/db.module';
 export * from './lib/prisma.service';
