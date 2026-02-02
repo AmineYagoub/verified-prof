17808535be1a31ac84c3052fad46f742751e1842
Amine Yagoub <yagoub.2.amine@gmail.com>
2026-02-02 15:27:20 +0100

    feat(config): add image remote patterns for external sources

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -10,6 +10,18 @@ const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
 };
 
 const plugins = [
