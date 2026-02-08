31e45a74648edd1958de9d6e971989054d0ef860
Amine Yagoub <yagoub.2.amine@gmail.com>
2026-02-09 00:25:31 +0100

    feat(web): enhance GitHubConnect component with loading state and redirect handling

--- apps/web/src/components/home/GitHubConnect.tsx
+++ apps/web/src/components/home/GitHubConnect.tsx
@@ -2,17 +2,28 @@
 
 import { signInWithGitHub } from '@verified-prof/web/lib/auth-client';
 import { ConnectIcon } from '../icons/ConnectIcon';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 interface GitHubConnectProps {
   text?: string;
 }
 
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export const GitHubConnect = ({
   text = 'Connect GitHub',
 }: GitHubConnectProps) => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const handleConnect = async () => {
+    setLoading(true);
     try {
-      await signInWithGitHub();
+      const res = await signInWithGitHub();
+      await sleep(1500);
+      if (res.data.redirect) {
+        router.push('/redirect');
+      }
     } catch (error) {
       return error;
     }
@@ -23,8 +34,14 @@ export const GitHubConnect = ({
       onClick={handleConnect}
       className="btn btn-primary btn-sm min-w-[150px]"
     >
-      <ConnectIcon />
-      <span>{text}</span>
+      {loading ? (
+        <span className="loading loading-spinner loading-sm"></span>
+      ) : (
+        <>
+          <ConnectIcon />
+          <span>{text}</span>
+        </>
+      )}
     </button>
   );
 };
